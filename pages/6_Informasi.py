5e8ebb56da35b1af8e80988677113afcab227b8a
ksophiena <kamilasophie20@gmail.com>
2026-07-20 21:18:39 +0700

    Update 6_Informasi.py

--- pages/6_Informasi.py
+++ pages/6_Informasi.py
@@ -11,7 +11,7 @@ from utils import auth, database as db
 from utils.theme import apply_theme
 from utils.components import render_sidebar, render_topbar, law_disclaimer
 
-st.set_page_config(page_title="TanyaHukum - Tentang", layout="wide")
+st.set_page_config(page_title="TanyaHukum - Tentang", layout="wide", initial_sidebar_state="collapsed")
 st.markdown("<style>[data-testid='stSidebarNav'] {display: none;}</style>", unsafe_allow_html=True)
 
 db.init_db()
